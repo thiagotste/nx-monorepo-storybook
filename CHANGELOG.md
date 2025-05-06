a7c51206c6b305673c115a3b518e7fe6c1e7f88f
Thiago Santos <thiago-f_4@outlook.com>
2025-05-06 17:59:28 -0300

    primeiro commit

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+## 0.1.9 (2025-05-06)
+
+This was a version bump only, there were no code changes.
+
+## 0.1.8 (2025-05-06)
+
+This was a version bump only, there were no code changes.
+
+## 0.1.6 (2025-05-05)
+
+This was a version bump only, there were no code changes.
+
+## 0.1.5 (2025-05-05)
+
+This was a version bump only, there were no code changes.
+
+## 0.1.0 (2025-05-04)
+
+This was a version bump only, there were no code changes.
---
+++ libs/button/src/lib/button/button.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+type Variant = 'primary' | 'secondary' | 'tertiary';
+type Theme = 'blue' | 'violet';
+
+@Component({
+  selector: 'ab-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  /**
+   * Texto do botão
+   */
+  @Input() text = '';
+  @Input() variant: Variant = 'primary';
+  @Input() disabled = false;
+  @Input() theme: Theme = 'blue';
+
+  getClass(): string {
+    return `${this.theme} ${this.variant}`;
+  }
+}
